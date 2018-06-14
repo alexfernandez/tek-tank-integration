e731cc09fdae5e5ccc4373b19e5dd2d14578f0fd
Alex Fernández <alexfernandeznpm@gmail.com>
2018-06-14 15:36:00 +0000

    Deploy well.

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ const server = http.createServer((request, response) => {
       response.writeHead(500)
       response.end('Integration failed: ' + stdout + stderr)
     } else {
-      childProcess.exec('ssh ubuntu@54.154.61.33 && cd tek-tank-demo && git pull', (error, stdout2, stderr2) => {
+      console.log('int ok')
+      childProcess.exec('ssh ubuntu@54.154.61.33 "cd tek-tank-demo && git pull"', (error, stdout2, stderr2) => {
         if (error) {
+          console.error('dep failed %s', stderr2)
           response.writeHead(500)
           response.end('Deployment failed: ' + stdout2 + stderr2)
         } else {
+          console.log('dep ok')
           response.end('Integration OK: ' + stdout + '\nDeployment OK: ' + stdout2)
         }
       })
