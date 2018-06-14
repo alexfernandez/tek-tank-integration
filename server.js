e5565ba47832d2aa6f3ff8235069e50d409c174d
Alex Fernández <alexfernandeznpm@gmail.com>
2018-06-14 15:01:53 +0000

    Send 500 on error.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const childProcess = require('child_process');
 const server = http.createServer((request, response) => {
   childProcess.exec('cd ../tek-tank-demo && git pull && npm test', (error, stdout, stderr) => {
     if (error) {
+      response.writeHead(500)
       response.end('Integration failed: ' + stdout + stderr)
     } else {
       response.end('Integration OK: ' + stdout)
