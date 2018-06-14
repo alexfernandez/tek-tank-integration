eedf77d635b571528b17c8b05310f32619078e24
Alex Fernández <alexfernandeznpm@gmail.com>
2018-06-14 14:50:53 +0000

    First version.

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const http = require('http');
+const childProcess = require('child_process');
+
+const server = http.createServer((request, response) => {
+  childProcess.exec('cd ../tek-tank-demo && git pull && npm test', (error, stdout, stderr) => {
+    if (error) {
+      response.end('Integration failed: ' + stdout + stderr)
+    } else {
+      response.end('Integration OK: ' + stdout)
+    }
+  })
+})
+server.listen(8000)
+
